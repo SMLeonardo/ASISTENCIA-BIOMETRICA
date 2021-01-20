27c2dbcc20f7b93c34a387520be885cf015dcbb5
SMLeonardo <way_leonardo@hotmail.com>
2021-01-20 00:05:18 -0500

    v1

---
+++ CONTROL_ASISTENCIA/src/CONTROLADOR/Conexion.java
@@ -0,0 +1,33 @@
+package CONTROLADOR;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.sql.*;
+import java.util.Scanner;
+import javax.swing.*;
+
+/**
+ *
+ * @author Sony
+ */
+
+public class Conexion {
+
+    Connection conect = null;
+
+    public Connection Conexion() {
+
+        try {
+
+            //Cargamos el Driver MySQL
+            Class.forName("com.mysql.jdbc.Driver");
+            conect = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/ELECTRO", "root", "12345678");
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error " + e);
+        }
+        return conect;
+
+    }
+}
